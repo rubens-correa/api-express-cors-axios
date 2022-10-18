525751db3fd4c56a6e221d7d169cd8183e71dc15
Rubens Correa <ozyres.dev@gmail.com>
2022-10-18 01:50:48 -0300

    api
    Consumindo dados do {JSON} Placeholder para minha API.

---
+++ server.js
@@ -0,0 +1,19 @@
+const cors = require ('cors')
+const express = require ('express')
+const app = express()
+const axios = require ('axios')
+
+app.use(cors())
+
+app.get('/', async(req, res) => {
+    try {
+        const {data} = await axios ('https://jsonplaceholder.typicode.com/users')
+        console.log (data)
+        return res.json(data)
+    } catch (error) {
+        console.error(error)
+    }
+   
+})
+
+app.listen('1010')
